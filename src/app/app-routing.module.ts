38d22e74d1010baabcffeae3c4d0fdc8b0c33fbc
Mats Andresen <mandresen@papego.de>
2018-04-04 19:48:31 +0200

    Drop redundant routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
 import { ImpressumComponent } from './impressum/impressum.component';
-import { ServicesComponent } from './services/services.component';
-import { ContactComponent } from './contact/contact.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
-  { path: 'about', component: LandingPageComponent },
-  { path: 'ueber-uns', component: LandingPageComponent },
-  { path: 'über-uns', component: LandingPageComponent },
-  { path: 'services', component: LandingPageComponent },
-  { path: 'dienstleistungen', component: LandingPageComponent },
-  { path: 'kontakt', component: LandingPageComponent },
-  { path: 'contact', component: LandingPageComponent },
   { path: 'impressum', component: ImpressumComponent }
 ];
 
