3c69bc6174540b1a4e772a4126070312128ee116
Mats Andresen <mandresen@papego.de>
2018-04-06 13:29:57 +0200

    Automate contact from.

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'landwerk-contact-form',
@@ -10,6 +10,8 @@ export class ContactFormComponent implements OnInit {
 
   private contactMessage: ContactMessage;
   private contactForm: FormGroup;
+
+  inputs: IFormInput[];
 
   constructor() { }
 
@@ -22,19 +24,49 @@ export class ContactFormComponent implements OnInit {
       'subject': new FormControl(this.contactMessage.subject, [Validators.required]),
       'message': new FormControl(this.contactMessage.message, [Validators.required])
     });
+    this.inputs = [
+      new FormInput('name', 'Name', 'Ihr Name', this.contactForm.get('name'), 'Bitte gib deinen Namen an.'),
+      new FormInput('email', 'E-Mail', 'Deine E-Mail', this.contactForm.get('email'), 'Gib eine gültige E-Mail-Adresse unter der wir dich kontaktieren können an.'),
+      new FormInput('phone', 'Telefonnummer', 'Deine Telefonnummer', this.contactForm.get('phone')),
+      new FormInput('subject', 'Betreff', 'Worum geht es?', this.contactForm.get('subject'), 'Gib eine kurze Beschreibung worum es geht.')
+    ];
   }
 
   get contact() { return this.contactForm; }
-  get name() { return this.contactForm.get('name'); }
-  get email() { return this.contactForm.get('email'); }
-  get phone() { return this.contactForm.get('phone'); }
-  get subject() { return this.contactForm.get('subject'); }
   get message() { return this.contactForm.get('message'); }
+
+  isInvalid(formControl: FormControl) {
+    return formControl.invalid && (formControl.dirty || formControl.touched)
+  }
 
   onSubmit() {
     if(!this.contactForm.invalid) {
       console.log(this.contactForm.value);
     }
+  }
+}
+
+export interface IFormInput {
+  name: string;
+  label: string;
+  placeholder: string;
+  feedback?: string;
+  formControl: AbstractControl;
+  hasErrors: boolean;
+}
+
+export class FormInput implements IFormInput {
+  constructor(
+    public name: string,
+    public label: string,
+    public placeholder: string,
+    public formControl: AbstractControl,
+    public feedback: string = null
+  )
+  {}
+
+  get hasErrors(): boolean {
+    return this.formControl.invalid && (this.formControl.dirty || this.formControl.touched);
   }
 }
 
