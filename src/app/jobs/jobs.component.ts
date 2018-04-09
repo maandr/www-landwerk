ecc1dbe2f67f3869b51910eebc4957053e9e51fd
Mats Andresen <mandresen@papego.de>
2018-04-09 10:18:20 +0200

    Add jobs-component.

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalFileService } from '../local-file.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'landwerk-jobs',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobsComponent implements OnInit {
 
-  constructor() { }
+  offers: JobOffer[];
+
+  constructor(
+    private localFileService: LocalFileService
+  ) {}
 
   ngOnInit() {
+    this.localFileService.getJson<JobsData>('jobs')
+      .subscribe(response => {
+        this.offers = response.offers;
+      });
   }
+}
 
+export interface JobsData {
+  offers: JobOffer[]
+}
+
+export interface JobOffer {
+  title: string,
+  description: string,
+  responsibilities: string[],
+  skills: {
+    profile: string
+    required: string[],
+    additional?: string[]
+  }
+  contact: string
 }
--- src/app/routing.service.ts
+++ src/app/routing.service.ts
@@ -11,12 +11,20 @@ export class RoutingService {
 ) {}
 
   open(rel: Relation) {
-    if(rel.anchor && this.router.url === rel.href) {
+    if(!rel.anchor) {
+      return this.router.navigateByUrl(rel.href);
+    }
+
+    const currentRoute = (this.router.url.indexOf('#'))
+      ? this.router.url.split('#')[0]
+      : this.router.url;
+
+    if(currentRoute === rel.href) {
       this.scrollService.scrollTo({
         target: rel.anchor
       });
     } else {
-      this.router.navigateByUrl(rel.href);
+      this.router.navigateByUrl(rel.href, { fragment: rel.anchor });
     }
   }
 }
